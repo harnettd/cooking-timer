57a4f8ac79d165d30866b5437a4ca2f27c19d65e
Derek Harnett <DHarnett.dev@proton.me>
2025-03-31 13:46:03 -0700

    stop timer at zero

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
 
-        this.startButton.addEventListener("click", this.start);
-        this.pauseButton.addEventListener("click", this.pause);
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    this.tick();
+    this.intervalId = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.intervalId);
+  };
+
+  onDurationChange = () => {};
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
     }
+  };
 
-    start = () => {
-        this.tick();
-        this.intervalId = setInterval(this.tick, 1000);
-    }
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
 
-    pause = () => {
-        clearInterval(this.intervalId);
-    }
-
-    onDurationChange = () => {}
-
-    tick = () => {
-        this.timeRemaining = this.timeRemaining - 1;
-    }
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
 }
 
 const durationInput = document.getElementById("duration");
