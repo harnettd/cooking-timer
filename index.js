e3baf3e47b6601ef515be5513361f6630d923456
Derek Harnett <DHarnett.dev@proton.me>
2025-03-31 14:14:51 -0700

    add skeleton Timer callbacks

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.startButton.addEventListener("click", this.start);
     this.pauseButton.addEventListener("click", this.pause);
   }
 
   start = () => {
+    this.onStart && this.onStart();
     this.tick();
     this.intervalId = setInterval(this.tick, 1000);
   };
@@ -22,8 +28,10 @@ class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      this.onComplete && this.onComplete();
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      this.onTick && this.onTick();
     }
   };
 
@@ -39,4 +47,14 @@ class Timer {
 const durationInput = document.getElementById("duration");
 const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Start");
+  },
+  onTick() {
+    console.log("Tick");
+  },
+  onComplete() {
+    console.log("Complete");
+  },
+});
