5ff491f0fe555a06f75ad4e6ff6979962be7dcb9
Derek Harnett <DHarnett.dev@proton.me>
2025-04-08 13:44:47 -0700

    add timer to timePanels

--- timerPanel.js
+++ timerPanel.js
@@ -1,43 +1,50 @@
 class TimerPanel {
-    constructor(id, panel, onDelTimerPanel) {
-        this.id = id;
-        this.panel = panel;
-        if (onDelTimerPanel) {
-            this.onDelTimerPanel = onDelTimerPanel;
-        }
-
-        this.input = document.createElement("input");
-        this.input.setAttribute("placeholder", "Untitled");
-        this.delButton = document.createElement("button");
-        this.delButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-        this.delButton.addEventListener("click", this.delTimerPanel);
-        this.display = document.createElement("input");
-        this.display.value = "1:00:00.0";
-        // this.progressMeter = document.createElement("p")
-        this.playButton = document.createElement("button");
-        this.playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
-        this.pauseButton = document.createElement("button");
-        this.pauseButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
-
-        const labelRow = document.createElement("div");
-        const displayRow = document.createElement("div");
-        const btnRow = document.createElement("div");
-
-        labelRow.appendChild(this.input);
-        labelRow.appendChild(this.delButton);
-        displayRow.appendChild(this.display);
-        btnRow.appendChild(this.playButton);
-        btnRow.appendChild(this.pauseButton);
-
-        this.panel.appendChild(labelRow);
-        this.panel.appendChild(displayRow);
-        this.panel.appendChild(btnRow);
+  constructor(id, panel, onDelTimerPanel) {
+    this.id = id;
+    this.panel = panel;
+    if (onDelTimerPanel) {
+      this.onDelTimerPanel = onDelTimerPanel;
     }
 
-    delTimerPanel = () => {
-        this.panel.nextSibling.remove();
-        this.panel.remove();
-        this.onDelTimerPanel && this.onDelTimerPanel();
-        // Should we delete the TimerPanel itself?
-    }
+    this.input = document.createElement("input");
+    this.input.setAttribute("placeholder", "Untitled");
+    this.delButton = document.createElement("button");
+    this.delButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    this.delButton.addEventListener("click", this.delTimerPanel);
+    this.display = document.createElement("input");
+    this.display.value = "60";
+    // this.progressMeter = document.createElement("p")
+    this.playButton = document.createElement("button");
+    this.playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+    this.pauseButton = document.createElement("button");
+    this.pauseButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    this.timer = new Timer(this.display, this.playButton, this.pauseButton, {
+      onStart() {},
+      onTick() {},
+      onComplete() {
+        console.log("Complete");
+      },
+    });
+
+    const labelRow = document.createElement("div");
+    const displayRow = document.createElement("div");
+    const btnRow = document.createElement("div");
+
+    labelRow.appendChild(this.input);
+    labelRow.appendChild(this.delButton);
+    displayRow.appendChild(this.display);
+    btnRow.appendChild(this.playButton);
+    btnRow.appendChild(this.pauseButton);
+
+    this.panel.appendChild(labelRow);
+    this.panel.appendChild(displayRow);
+    this.panel.appendChild(btnRow);
+  }
+
+  delTimerPanel = () => {
+    this.panel.nextSibling.remove();
+    this.panel.remove();
+    this.onDelTimerPanel && this.onDelTimerPanel();
+    // Should we delete the TimerPanel itself?
+  };
 }
